f79f7f90229ea17c33247061db5be03b40171eaa
Kevy-k <158181402+Kevy-k@users.noreply.github.com>
2026-02-24 10:12:37 +0530

    Update theme.dart

--- lib/screens/theme.dart
+++ lib/screens/theme.dart
@@ -1,22 +1,36 @@
 import 'package:flutter/material.dart';
 
 ThemeData darkMode = ThemeData(
-  brightness: Brightness.light,
-  colorScheme: const ColorScheme.light(
-   surface :  Color.fromARGB(255, 37, 44, 51),
+  brightness: Brightness.dark,
+  
+  textSelectionTheme: TextSelectionThemeData(
+    selectionColor: Colors.blue.withOpacity(0.3),
+    selectionHandleColor: Colors.blue,            
+    cursorColor: Colors.blue,                  
+  ),
+   
+  
+  colorScheme: const ColorScheme.dark(
+    surface :  Color.fromARGB(255, 37, 44, 51),
     primary:  Color.fromARGB(255, 37, 44, 51),
     secondary:Color.fromARGB(255, 220, 220, 220),
-    tertiary: Color.fromARGB(255, 255, 94, 58),
-    
+    tertiary: Color.fromARGB(255, 255, 94, 58), 
   ),
 );
+
 ThemeData lightMode = ThemeData(
   brightness: Brightness.light,
+  
+  textSelectionTheme: TextSelectionThemeData(
+    selectionColor: Colors.blueAccent.withOpacity(0.4),
+    selectionHandleColor: Colors.blueAccent,
+    cursorColor: Colors.blueAccent,
+  ),
+   
   colorScheme: const ColorScheme.light(
-   surface :  Color.fromARGB(255, 220, 220, 220),
+    surface :  Color.fromARGB(255, 220, 220, 220),
     primary:  Color.fromARGB(255, 220, 220, 220),
     secondary:Color.fromARGB(255, 37, 44, 51),
     tertiary: Color.fromARGB(255, 255, 94, 58),
-    
   ),
 );
