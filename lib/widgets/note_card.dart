1cf636b8ad3d8db8b061a956f2e285f458b70cfe
Kevy-k <158181402+Kevy-k@users.noreply.github.com>
2026-02-24 10:12:03 +0530

    Create note_card.dart

---
+++ lib/widgets/note_card.dart
@@ -0,0 +1,95 @@
+import 'package:campus_notes/screens/view_notes.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:campus_notes/models/note_model.dart';
+
+class NoteCard extends StatelessWidget {
+  final Note note;
+  final VoidCallback onDelete;
+  final VoidCallback onEdit;
+
+  const NoteCard({
+    super.key,
+    required this.note,
+    required this.onDelete,
+    required this.onEdit,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Theme.of(context).colorScheme.secondary,
+      elevation: 3,
+      margin: EdgeInsets.symmetric(horizontal: 10.w, vertical: 8.h),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12.r),
+      ),
+      child: InkWell(
+        onTap: ()=>Navigator.push(context, MaterialPageRoute(builder: (context)=>ViewNotes(note: note))),
+        child: Padding(
+          padding: EdgeInsets.all(12.w),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: Text(
+                      note.title,
+                      style: TextStyle(
+                        fontSize: 18.sp,
+                        fontWeight: FontWeight.bold,
+                        color: Theme.of(context).colorScheme.primary
+                      ),
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis, // Adds "..." if too long
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      IconButton(
+                        icon: const Icon(Icons.edit, color: Colors.blue),
+                        onPressed: onEdit,
+                      ),
+                      IconButton(
+                        icon: const Icon(Icons.delete, color: Colors.red),
+                        onPressed: onDelete,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              
+              //subject
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 8.w, vertical: 4.h),
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.primary.withOpacity(0.1),
+                  borderRadius: BorderRadius.circular(8.r),
+                ),
+                child: Text(
+                  note.subject,
+                  style: TextStyle(fontSize: 12.sp, color: Colors.blue),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+              
+              SizedBox(height: 10.h),
+            
+              // Description 
+              Text(
+                note.description,
+                style: TextStyle(fontSize: 14.sp, color: Theme.of(context).colorScheme.primary),
+                maxLines: 2, // Only show first two lines on the dashboard
+                overflow: TextOverflow.ellipsis,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
